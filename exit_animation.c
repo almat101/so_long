28271f8243b70dc033788e337885765c41146213
Alessandro Matta <amatta@e3r4p8.42roma.it>
2023-07-31 16:15:44 +0200

    increased animation time

--- exit_animation.c
+++ exit_animation.c
@@ -6,7 +6,7 @@
 /*   By: amatta <amatta@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/31 12:50:43 by amatta            #+#    #+#             */
-/*   Updated: 2023/07/31 15:59:59 by amatta           ###   ########.fr       */
+/*   Updated: 2023/07/31 16:14:07 by amatta           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ int	put_exit_animation(t_game *game)
 	if (game->collect_count == game->count_collect)
 	{
 		game->frame += 1;
-		if (game->frame <= 5000)
+		if (game->frame <= 10000)
 			mlx_put_image_to_window(game->mlx, game->win, game->exit1,
 				game->e_x * 64, game->e_y * 64);
-		if (game->frame > 5000 && game->frame <= 10000)
+		if (game->frame > 10000 && game->frame <= 20000)
 			mlx_put_image_to_window(game->mlx, game->win, game->exit2,
 				game->e_x * 64, game->e_y * 64);
-		if (game->frame > 10000 && game->frame <= 15000)
+		if (game->frame > 20000 && game->frame <= 30000)
 			mlx_put_image_to_window(game->mlx, game->win, game->exit3,
 				game->e_x * 64, game->e_y * 64);
-		if (game->frame > 15000)
+		if (game->frame > 30000)
 			game->frame = -1;
 	}
 	return (0);
